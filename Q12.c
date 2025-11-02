39cf5c54de8185dda59efc8a65519c8037b2640f
sowmyamulukutla26 <sree25csu390@ncuindia.edu>
2025-11-02 20:49:00 +0530

    Q12.c

---
+++ Q12.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, arr[20];
+    printf("Enter array size: ");
+    scanf("%d", &n);
+    printf("Enter elements: ");
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    int max = arr[0], min = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) max = arr[i];
+        if (arr[i] < min) min = arr[i];
+    }
+
+    printf("Max = %d, Min = %d\n", max, min);
+    return 0;
+}
